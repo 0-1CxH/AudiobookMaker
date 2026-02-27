be31d084369092e5cf9723c63b6df5a7d41b46f1
qhu <h0.1c@foxmail.com>
2026-02-27 21:20:16 +0800

    tested voice mgr

--- tmp.py
+++ tmp.py
@@ -1,5 +1,6 @@
 from src.text import TextManager
 from src.character import CharacterManager
+from src.voice import VoiceManager
 from src.utils import single_llm_request, extract_json
 
 if __name__ == "__main__":
@@ -13,3 +14,17 @@ if __name__ == "__main__":
     # print(cm.characters)
     # print(single_llm_request(model="aliyun/DeepSeek-V3.2", prompt="Hello world"))
     print(extract_json("adhaoifh```json\n{\"a\": 1}\n```"))
+    vm = VoiceManager(
+        voice_lib_folder_path="./voice_lib",
+        design_model_path="../models/Qwen3-TTS-VD",
+        clone_model_path="../models/Qwen3-TTS-Base",
+        use_flash_attention=True
+    )
+    vm.add_voice_design(
+        voice_name="test",
+        tts_instruction="音色特点：温暖柔和，语调：平稳自然，语速：适中，情感：友好亲切"
+    )
+    print(vm.voice_designs)
+    vm.generate_reference_audio(voice_name="test")
+    vm.generate_voice(voice_name="test", text="这是一个测试。", voice_save_path="./voice_lib/tmp_test.wav")
+    
